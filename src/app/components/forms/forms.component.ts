b4cab0c7bd97eb5d2d7767b02a5f6fb89dbd038a
Marcosjr <mjworks2020@outlook.com>
2022-04-17 00:01:19 -0300

    Added reactive form

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
-  styleUrls: ['./forms.component.scss']
+  styleUrls: ['./forms.component.scss'],
 })
 export class FormsComponent implements OnInit {
-
-  constructor() { }
+  userInformationsForm!: FormGroup;
+  sendDatas = false;
 
   ngOnInit(): void {
+    this.userInformationsForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      surname: new FormControl('', Validators.required),
+      age: new FormControl(null, Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
   }
 
+  onSubmit() {
+    this.sendDatas = true;
+  }
+
+  getDatas() {
+    if (this.userInformationsForm.valid) {
+      return {
+        // @ts-ignore
+        name: this.userInformationsForm.controls?.name?.value,
+        // @ts-ignore
+        surname: this.userInformationsForm.controls?.surname?.value,
+        // @ts-ignore
+        age: this.userInformationsForm.controls?.age?.value,
+        // @ts-ignore
+        email: this.userInformationsForm.controls?.email?.value,
+      };
+    }
+    return;
+  }
+
+  cleanForm() {
+    this.userInformationsForm.reset();
+    this.sendDatas = false;
+  }
 }
